270127e12d8f3f7d6017712f337090ddc9561a22
sorooshsj <soroush.sanjarani@gmail.com>
2018-12-01 00:38:22 +0330

    fixing UI

--- AttendanceApp/AttendanceApp/Models/InOut.cs
+++ AttendanceApp/AttendanceApp/Models/InOut.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,15 @@ namespace AttendanceApp.Models
     public class inout
     {
         public int Id { get; set; }
+        [Display(Name ="تاریخ")]
         public DateTime date { get; set; }
+        [Display(Name = "آیدی کارمند")]
         public string personId { get; set; }
+        [Display(Name = "زمان شروع")]
         public DateTime startTime { get; set; }
+        [Display(Name = "زمان پایان")]
         public DateTime endTime { get; set; }
+        [Display(Name = "در مرخصی")]
         public bool isRest { get; set; }
 
         /*
@@ -46,6 +52,7 @@ namespace AttendanceApp.Models
             }
 
         }
+        [Display(Name = "تایید شده؟")]
         public bool isCommited { get; set; }
         public ICollection<ApplicationUser> person { get; set; }
     }
--- AttendanceApp/AttendanceApp/Models/Rest.cs
+++ AttendanceApp/AttendanceApp/Models/Rest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,18 @@ namespace AttendanceApp.Models
     public class rest
     {
         public int Id { get; set; }
+        [Display(Name = "تاریخ شروع")]
         public DateTime startDate { get; set; }
+        [Display(Name = "تاریخ پایان")]
         public DateTime endDate { get; set; }
+        [Display(Name = "زمان شروع")]
         public DateTime startTime { get; set; }
+        [Display(Name = "زمان پایان")]
         public DateTime endTime { get; set; }
+        [Display(Name = "تایید شده؟")]
         public bool isCommited { get; set; }
         public ICollection<ApplicationUser> persons { get; set; }
+        [Display(Name = "جنسیت")]
         public string personId { get; set; }
     }
 }
